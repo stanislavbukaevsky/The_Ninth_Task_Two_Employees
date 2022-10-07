0e511acee20400aecb97e2fa4d3807256e9fd6c6
stanislavi18061989 <96617178+stanislavbukaevsky@users.noreply.github.com>
2022-10-07 14:55:37 +0300

    Первый коммит

---
+++ src/main/java/pro/sky/employees2/Employees2Application.java
@@ -0,0 +1,13 @@
+package pro.sky.employees2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Employees2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Employees2Application.class, args);
+	}
+
+}
